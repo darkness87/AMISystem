3f0542b08c31b8cb8f4982cd11686ce8baa236e1
LeePyeongBok <pblee@cnuglobal.com>
2021-02-05 18:23:28 +0900

    초안작성
    Signed-off-by: LeePyeongBok <pblee@cnuglobal.com>

---
+++ Ami/src/main/java/com/cnu/ami/AmiApplication.java
@@ -0,0 +1,13 @@
+package com.cnu.ami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmiApplication.class, args);
+	}
+
+}
