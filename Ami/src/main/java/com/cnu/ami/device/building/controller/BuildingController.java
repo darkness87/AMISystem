9d3d13d70444bfdec264b990ce43a60bd29af15f
sookwon <skchae@cnuglobal.com>
2021-04-09 15:23:30 +0900

    #7 => @RequestParam(required = false, defaultValue = "0") 적용 #8 => DCU 설정 라이브러리 jar API 변경

--- Ami/src/main/java/com/cnu/ami/device/building/controller/BuildingController.java
+++ Ami/src/main/java/com/cnu/ami/device/building/controller/BuildingController.java
@@ -45,9 +45,9 @@ public class BuildingController {
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.OK)
 	@Description(value = "설비:동관리 : 동 리스트정보")
-	public Mono<ResponseListVO<BuildingVO>> getBuildingListData(HttpServletRequest request, @RequestParam int regionSeq, @RequestParam int estateSeq)
+	public Mono<ResponseListVO<BuildingVO>> getBuildingListData(HttpServletRequest request, @RequestParam(required = false, defaultValue = "0") int regionSeq, @RequestParam(required = false, defaultValue = "0") int estateSeq)
 			throws Exception {
-
+		
 		List<BuildingVO> data = buildingService.getBuildingListData(regionSeq,estateSeq);
 
 		return Mono.just(new ResponseListVO<BuildingVO>(request, data));
