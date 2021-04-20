a86f8cf85ad37200c1acb211d14ac9b2d43853fd
sookwon <skchae@cnuglobal.com>
2021-04-20 13:51:42 +0900

    token 임시 미 처리

--- Ami/src/main/java/com/cnu/ami/security/SecurityConfig.java
+++ Ami/src/main/java/com/cnu/ami/security/SecurityConfig.java
@@ -24,7 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.csrf().disable() // csrf 사용 안 함 == REST API 사용하기 때문에
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // JWT인증사용하므로 세션 사용 함
 				.and().authorizeRequests() // 다음 Request에 대한 사용권한 체크
-				.antMatchers("/api/login", "/api/registration", "/api/getApiList", "/api/*/test", "/api/*/test/*", "/api/*/test/*/*", "/api/*/*/test/*", "/api/*/*/test/*/*")
+//				.antMatchers("/api/login", "/api/registration", "/api/getApiList", "/api/*/test", "/api/*/test/*", "/api/*/test/*/*", "/api/*/*/test/*", "/api/*/*/test/*/*")
+				.antMatchers("/api/*","/api/*/*", "/api/*/*/*", "/api/*/*/*/*", "/api/*/*/*/*/*", "/api/*/*/*/*/*/*") // token 없이 테스트용
 				.permitAll() // 가입 및 인증 주소는 누구나 접근가능 // 추후 누구나 접근가능한 컨트롤러 설정 후 /* 표시로 한번에 설정 고려
 //	  		.anyRequest().hasRole("ADMIN") // 그외 나머지 요청은 모두 인증된 회원만 접근 가능
 				.anyRequest().hasAnyRole("ADMIN", "OPER", "USER") // 추후 hasRole , hasAnyRole 에 따라 API 접근 권한 변경
