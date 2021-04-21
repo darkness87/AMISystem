221a0493476f6c39da59224fae56f920aae5ca55
sookwon <skchae@cnuglobal.com>
2021-04-21 14:01:16 +0900

    #2 현황판 지도 - 램덤 값 임시 지정

--- Ami/src/main/java/com/cnu/ami/dashboard/service/impl/DashBoardServiceImpl.java
+++ Ami/src/main/java/com/cnu/ami/dashboard/service/impl/DashBoardServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -347,60 +348,63 @@ public class DashBoardServiceImpl implements DashBoardService {
 
 		List<DashBoardMapVO> dashmap = new ArrayList<DashBoardMapVO>();
 		DashBoardMapVO dashBoardMapVO = new DashBoardMapVO();
-
+		
 		for (int i = 0; i < 17; i++) {
 			dashBoardMapVO = new DashBoardMapVO();
+			
+			Random random = new Random();
+			
 			if (i == 0) {
 				dashBoardMapVO.setHckey("kr-so"); // 서울특별시
-				dashBoardMapVO.setValue(5);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 1) {
 				dashBoardMapVO.setHckey("kr-pu"); // 부산광역시
-				dashBoardMapVO.setValue(7);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 2) {
 				dashBoardMapVO.setHckey("kr-tg"); // 대구광역시
-				dashBoardMapVO.setValue(10);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 3) {
 				dashBoardMapVO.setHckey("kr-in"); // 인천광역시
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 4) {
 				dashBoardMapVO.setHckey("kr-kj"); // 광주광역시
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 5) {
 				dashBoardMapVO.setHckey("kr-tj"); // 대전광역시
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 6) {
 				dashBoardMapVO.setHckey("kr-ul"); // 울산광역시
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 7) {
 				dashBoardMapVO.setHckey("kr-kg"); // 경기도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 8) {
 				dashBoardMapVO.setHckey("kr-kw"); // 강원도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 9) {
 				dashBoardMapVO.setHckey("kr-gb"); // 충청북도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 10) {
 				dashBoardMapVO.setHckey("kr-gn"); // 충청남도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 11) {
 				dashBoardMapVO.setHckey("kr-cb"); // 전라북도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 12) {
 				dashBoardMapVO.setHckey("kr-2685"); // 전라남도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 13) {
 				dashBoardMapVO.setHckey("kr-2688"); // 경상북도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 14) {
 				dashBoardMapVO.setHckey("kr-kn"); // 경상남도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 15) {
 				dashBoardMapVO.setHckey("kr-cj"); // 제주특별자치도
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			} else if (i == 16) {
 				dashBoardMapVO.setHckey("kr-sj"); // 세종특별자치시
-				dashBoardMapVO.setValue(3);
+				dashBoardMapVO.setValue(random.nextInt(20));
 			}
 
 			dashmap.add(dashBoardMapVO);
