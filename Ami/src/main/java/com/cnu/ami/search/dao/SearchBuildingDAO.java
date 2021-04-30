90246b0d901b3b6ee41fe9f016abf7ffd06485d9
sookwon <skchae@cnuglobal.com>
2021-04-30 16:21:37 +0900

    #26 삭제 dcu 관련 처리

--- Ami/src/main/java/com/cnu/ami/search/dao/SearchBuildingDAO.java
+++ Ami/src/main/java/com/cnu/ami/search/dao/SearchBuildingDAO.java
@@ -15,13 +15,13 @@ public interface SearchBuildingDAO extends JpaRepository<BuildingEntity, Long> {
 
 	public List<BuildingEntity> findByGSEQ(int gseq);
 
-	@Query(value = "SELECT T1.BSEQ,T1.BNAME,T2.DID FROM (SELECT BSEQ,BNAME FROM BUILDING) AS T1\r\n" + 
-			"JOIN BUILDING_DCU_MAP AS T2\r\n" + 
-			"ON T1.BSEQ=T2.BSEQ", nativeQuery = true)
+	@Query(value = "SELECT T1.BSEQ,T1.BNAME,T2.DID FROM (SELECT BSEQ,BNAME FROM BUILDING) AS T1\r\n"
+			+ "JOIN BUILDING_DCU_MAP AS T2\r\n" + "ON T1.BSEQ=T2.BSEQ"
+			+ "JOIN (SELECT DID FROM DCU_INFO WHERE IS_DELETE='N') T3\r\n" + "ON T2.DID=T3.DID", nativeQuery = true)
 	public List<BuildingDcuInterfaceVO> getBuildingDcuList();
-	
-	@Query(value = "SELECT T1.BSEQ,T1.BNAME,T2.DID FROM (SELECT BSEQ,BNAME FROM BUILDING WHERE GSEQ = :gseq) AS T1\r\n" + 
-			"JOIN BUILDING_DCU_MAP AS T2\r\n" + 
-			"ON T1.BSEQ=T2.BSEQ", nativeQuery = true)
+
+	@Query(value = "SELECT T1.BSEQ,T1.BNAME,T2.DID FROM (SELECT BSEQ,BNAME FROM BUILDING WHERE GSEQ = :gseq) AS T1\r\n"
+			+ "JOIN BUILDING_DCU_MAP AS T2\r\n" + "ON T1.BSEQ=T2.BSEQ"
+			+ "JOIN (SELECT DID FROM DCU_INFO WHERE IS_DELETE='N') T3\r\n" + "ON T2.DID=T3.DID", nativeQuery = true)
 	public List<BuildingDcuInterfaceVO> getBuildingDcuList(@Param("gseq") int gseq);
 }
