ef6fcb0dc7b81007430ef2558cf3e00b6fc60969
sookwon <skchae@cnuglobal.com>
2021-04-30 17:23:34 +0900

    #5 검색 동 부분 오류 수정

--- Ami/src/main/java/com/cnu/ami/search/dao/SearchBuildingDAO.java
+++ Ami/src/main/java/com/cnu/ami/search/dao/SearchBuildingDAO.java
@@ -21,7 +21,7 @@ public interface SearchBuildingDAO extends JpaRepository<BuildingEntity, Long> {
 	public List<BuildingDcuInterfaceVO> getBuildingDcuList();
 
 	@Query(value = "SELECT T1.BSEQ,T1.BNAME,T2.DID FROM (SELECT BSEQ,BNAME FROM BUILDING WHERE GSEQ = :gseq) AS T1\r\n"
-			+ "JOIN BUILDING_DCU_MAP AS T2\r\n" + "ON T1.BSEQ=T2.BSEQ"
+			+ "JOIN BUILDING_DCU_MAP AS T2\r\n" + "ON T1.BSEQ=T2.BSEQ\r\n"
 			+ "JOIN (SELECT DID FROM DCU_INFO WHERE IS_DELETE='N') T3\r\n" + "ON T2.DID=T3.DID", nativeQuery = true)
 	public List<BuildingDcuInterfaceVO> getBuildingDcuList(@Param("gseq") int gseq);
 }
